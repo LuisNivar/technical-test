619e7e0e0af82880505143e2dc8556a2a0365f5c
LuisNivar <luisjosenivarn@hotmail.com>
2025-02-24 17:05:53 -0400

    Move handleSubmit to TaskForm component

--- 01-todo-list/src/App.tsx
+++ 01-todo-list/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Item from "./components/Item";
+import { ListTasks } from "./components/ListTasks";
 import { TaskForm } from "./components/TaskForm";
 import { useTasks } from "./hooks/useTasks";
 
@@ -11,21 +11,7 @@ export type TaskType = {
 function App() {
   const { tasks, AddTask, RemoveTask } = useTasks();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const { elements } = e.currentTarget;
-    const input = elements.namedItem("input");
-    const isInput = input instanceof HTMLInputElement;
-
-    if (!isInput || input === null) return;
-
-    AddTask(input.value);
-
-    input.value = "";
-  };
-
-  const createHandleRemove = (id: TaskType["id"]) => () => {
+  const HandleRemove = (id: TaskType["id"]) => {
     RemoveTask(id);
   };
 
@@ -34,8 +20,7 @@ function App() {
       <aside>
         <h1>TODO List</h1>
         <p>Add task to do that can be deleted</p>
-
-        <TaskForm onSubmit={handleSubmit} />
+        <TaskForm AddTask={AddTask} />
       </aside>
 
       <section>
@@ -44,15 +29,7 @@ function App() {
         {tasks.length === 0 ? (
           <p>Empty list, add something...</p>
         ) : (
-          <ul>
-            {tasks.map((task) => (
-              <Item
-                key={task.id}
-                {...task}
-                onRemove={createHandleRemove(task.id)}
-              />
-            ))}
-          </ul>
+          <ListTasks tasks={tasks} onRemove={HandleRemove} />
         )}
       </section>
     </main>
