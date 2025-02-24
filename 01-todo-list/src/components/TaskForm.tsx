5337011bb6afb23f0b41c75efa39d435d8c7a002
LuisNivar <45906349+LuisNivar@users.noreply.github.com>
2025-02-24 17:08:10 -0400

    01 refactor task list and form (#3)
    * Add ListTasks component
    
    * Move handleSubmit to TaskForm component

--- 01-todo-list/src/components/TaskForm.tsx
+++ 01-todo-list/src/components/TaskForm.tsx
@@ -1,10 +1,26 @@
+import { TaskType } from "../App";
+
 type TaskFormProps = {
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  AddTask: (text: TaskType["text"]) => void;
 };
 
-export function TaskForm({ onSubmit }: TaskFormProps) {
+export function TaskForm({ AddTask }: TaskFormProps) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { elements } = e.currentTarget;
+    const input = elements.namedItem("input");
+    const isInput = input instanceof HTMLInputElement;
+
+    if (!isInput || input === null) return;
+
+    AddTask(input.value);
+
+    input.value = "";
+  };
+
   return (
-    <form onSubmit={onSubmit} aria-label="Add items">
+    <form onSubmit={handleSubmit} aria-label="Add items">
       <label htmlFor="input">
         <input
           type="text"
