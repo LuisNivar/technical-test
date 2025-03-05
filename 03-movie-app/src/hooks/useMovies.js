17a16066825a4bb664cd8ec2b6c0cdc85409690b
LuisNivar <luisjosenivarn@hotmail.com>
2025-03-05 16:11:18 -0400

    Prevent same search

--- 03-movie-app/src/hooks/useMovies.js
+++ 03-movie-app/src/hooks/useMovies.js
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { searchMovie } from "../services/movies";
 
 export function useMovies({ search }) {
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const previousSearch = useRef(search);
 
   const updateMovies = async () => {
+    if (previousSearch.current === search) return;
     try {
       setIsLoading(true);
       setError(null);
 
+      previousSearch.current = search;
       const newMovie = await searchMovie({ search });
       setMovies(newMovie);
     } catch (error) {
