010b6a986ebf21df1a7f3350fbaa39e97ac14dfd
LuisNivar <luisjosenivarn@hotmail.com>
2025-03-06 19:15:23 -0400

    Add basic layout

--- 04-shopping-cart/src/App.tsx
+++ 04-shopping-cart/src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import { Products } from "./components/Products";
 
 function App() {
   return (
     <>
-      <h1>Shopping Cart 🛒</h1>
+      <header>
+        <h1>Shopping Cart 🛒</h1>
+      </header>
+      <main>
+        <Products />
+      </main>
     </>
   );
 }
