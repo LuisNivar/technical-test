bf827f215f9df46147961ca9ff9ecd504f58c2a0
LuisNivar <luisjosenivarn@hotmail.com>
2025-03-08 16:16:54 -0400

    -Use filter component -Add some style

--- 04-shopping-cart/src/App.tsx
+++ 04-shopping-cart/src/App.tsx
@@ -1,14 +1,25 @@
 import "./App.css";
 import { Products } from "./components/Products";
 
+import { Filters } from "./components/Filters";
+import { useFilter } from "./hooks/useFilter";
+
 function App() {
+  const { filter, updateCategory, updateMaxValue } = useFilter();
+
   return (
     <>
       <header>
         <h1>Shopping Cart 🛒</h1>
+
+        <Filters
+          filter={filter}
+          onMaxValueChange={updateMaxValue}
+          onCategoryChange={updateCategory}
+        />
       </header>
       <main>
-        <Products />
+        <Products filter={filter} />
       </main>
     </>
   );
