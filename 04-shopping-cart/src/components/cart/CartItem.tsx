c1ea38241e10604f644b4587fc3214479c4c11f5
LuisNivar <luisjosenivarn@hotmail.com>
2025-03-09 13:44:32 -0400

    Add Cart components

---
+++ 04-shopping-cart/src/components/cart/CartItem.tsx
@@ -0,0 +1,28 @@
+import { CartItemType } from ".";
+
+export type CartItemProps = CartItemType & {
+  onUpdateQuantity: (id: number) => void;
+  onRemove: (id: number) => void;
+};
+
+export function CartItem({ ...props }: CartItemProps) {
+  const { id, image, title, price, quantity, onUpdateQuantity, onRemove } =
+    props;
+
+  return (
+    <li className="cart-item">
+      <span>
+        <img src={image} alt={`Image of ${title}`} />
+        <button onClick={() => onRemove(id)}>❌</button>
+      </span>
+
+      <p>
+        {title} - $ {price}
+      </p>
+      <span>
+        Qty: {String(quantity).padStart(2, "0")}{" "}
+        <button onClick={() => onUpdateQuantity(id)}>+</button>
+      </span>
+    </li>
+  );
+}
