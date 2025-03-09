a716616f826c75b5ff25fd8d80a97f00dc67a382
LuisNivar <luisjosenivarn@hotmail.com>
2025-03-09 13:42:32 -0400

    Add cart types

--- 04-shopping-cart/src/type.d.ts
+++ 04-shopping-cart/src/type.d.ts
@@ -16,3 +16,24 @@ export type FiltersProps = {
   onMaxValueChange: (value: number) => void;
   onCategoryChange: (value: string) => void;
 };
+
+export type AddToCart = {
+  type: "Add";
+  product: ProductType;
+};
+
+export type RemoveItem = {
+  type: "Remove";
+  id: number;
+};
+
+export type ClearCart = {
+  type: "Clear";
+};
+
+export type UpdateQuantity = {
+  type: "Update";
+  id: number;
+};
+
+export type ActionType = AddToCart | RemoveItem | ClearCart | UpdateQuantity;
