10148f3ec29016e127e163f43d55b43b834aee84
LuisNivar <luisjosenivarn@hotmail.com>
2025-03-11 18:14:34 -0400

    Add initial files 05-user-table

---
+++ 05-user-table/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Top 100 Users 🙎‍♂️🙎‍♀️</h1>
+      </header>
+      <main>
+        <p>table</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
