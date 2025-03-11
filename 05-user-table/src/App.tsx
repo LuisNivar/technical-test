2b4b5108a026d4654b1985e46ef5928261a8495d
LuisNivar <luisjosenivarn@hotmail.com>
2025-03-11 19:07:18 -0400

    -Use ListOfUsers component -Fetching de data -Button to colored

--- 05-user-table/src/App.tsx
+++ 05-user-table/src/App.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ListOfUsers } from "./components/ListOfUsers";
+import { API_URL } from "./config";
+import { User } from "./types";
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [colored, setColored] = useState(false);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setUsers(data.results));
+  }, []);
+
+  const toggleColor = () => {
+    setColored(!colored);
+  };
+
   return (
     <>
       <header>
         <h1>Top 100 Users 🙎‍♂️🙎‍♀️</h1>
+        <span>
+          <button onClick={() => toggleColor()}>
+            {colored ? "Uncolored rows" : "Colored rows"}
+          </button>
+        </span>
       </header>
       <main>
-        <p>table</p>
+        <ListOfUsers colored={colored} users={users} />
       </main>
     </>
   );
