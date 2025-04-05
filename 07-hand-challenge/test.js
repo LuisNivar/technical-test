76dd0cefbd4381e58e66474ccabf79d90d12e37c
LuisNivar <45906349+LuisNivar@users.noreply.github.com>
2025-04-04 21:14:33 -0400

    07 hand challenge (#19)
    * Add Readme
    
    * -Install eslint + prettier
    -Install jes
    
    * Add gitignore
    
    * Configure eslint + prettier
    
    * Add test
    
    * Add solution

---
+++ 07-hand-challenge/test.js
@@ -0,0 +1,18 @@
+/*global test, expect */
+const translate = require("./index.js");
+
+test("First case 'Hello'", () => {
+  expect(
+    translate(
+      "👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊"
+    )
+  ).toBe("Hello");
+});
+
+test.skip("Second case 'Hello World!'", () => {
+  expect(
+    translate(
+      "👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊"
+    )
+  ).toBe("Hello World!");
+});
